25e065156428f1e1390a054fbb7e666cac414c26
Jannis R <mail@jannisr.de>
2016-08-19 00:56:18 +0200

    rewrite using dyndns-client, part 1

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const http = require('http')
+const cfg = require('config')
+const url = require('url')
+const qs = require('querystring')
+const dyndns = require('dyndns')(cfg.server.hostname, cfg.server.port, cfg.server.key)
+
+
+
+const server = http.createServer((req, res) => {
+	const err = (message, code) => {
+		if (!message) message = 'Unknown Error'
+		console.error(message)
+		res.statusCode = code || 500
+		return res.end(message)
+	}
+
+	const query = qs.parse(url.parse(req.url).query)
+	if (!query || !query.ip) return err('Missing IP address.', 400)
+	const ip = query.ip
+
+	dyndns('A', ip)
+	.then(
+		(ip) => console.log(cfg.domain + ' -> ' + ip),
+		() => err('Something went wrong.', 500))
+})
+
+server.listen(cfg.port, (err) => {
+	if (err) return console.error(err)
+	console.info(`Server listening on ${cfg.port}.`)
+})
