9c1b4826dda7f5f7bd29c6c58dce94b9635e751f
Jannis R <mail@jannisr.de>
2016-08-20 22:06:08 +0200

    let server query public ips by itself

--- index.js
+++ index.js
@@ -5,31 +5,35 @@ const cfg = require('config')
 const url = require('url')
 const qs = require('querystring')
 const dyndns = require('dyndns-client')
+const publicIp = require('public-ip')
 
 const update = dyndns(cfg.server.hostname, cfg.server.port, cfg.server.key)
 
 
 
 const server = http.createServer((req, res) => {
-	const err = (message, code) => {
-		if (!message) message = 'Unknown Error'
+	const err = (err) => {
+		const message = err.message || 'Unknown Error'
 		console.error(message)
-		res.statusCode = code || 500
-		return res.end(message)
+		res.statusCode = err.statusCode || 500
+		res.end(message)
+		throw err
 	}
 
-	const query = qs.parse(url.parse(req.url).query)
-	if (!query || !query.ip) return err('Missing IP address.', 400)
-	const ip = query.ip
 
-	update('A', ip)
-	.then(
-		(ip) => {
-			console.log(ip)
-			res.statusCode = 202
-			res.end('Success.')
-		},
-		(e) => err(e.message, 500))
+	Promise.all([
+		publicIp.v4(),
+		publicIp.v6()
+	])
+	.then(([ipv4, ipv6]) => Promise.all([
+		update('A', ipv4),
+		update('AAAA', ipv6)
+	]), err)
+	.then(([ipv4, ipv6]) => {
+		console.log(['v4', ipv4, 'v6', ipv6].join('\t'))
+		res.statusCode = 202
+		res.end('Success.')
+	}, err)
 })
 
 server.listen(cfg.port, (err) => {
